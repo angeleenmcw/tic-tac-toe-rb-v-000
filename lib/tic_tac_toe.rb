d0ca29085e908f17355014deae4329b5f4d565cc
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-25 06:23:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to-
+def input_to_index
+  input
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board)
 
